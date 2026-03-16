3638bbeccaafa3c00258ca3dbb60d204625e4984
doubleyuu6-blip <doubleyuu6@gmail.com>
2026-03-16 15:38:52 +0900

    Add files via upload

---
+++ code/1926098 유지환 DB weeks2.sql
@@ -0,0 +1,14 @@
+-- Database: exercise weeks2
+
+-- DROP DATABASE IF EXISTS "exercise weeks2";
+
+CREATE DATABASE "exercise weeks2"
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'Korean_Korea.949'
+    LC_CTYPE = 'Korean_Korea.949'
+    LOCALE_PROVIDER = 'libc'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
