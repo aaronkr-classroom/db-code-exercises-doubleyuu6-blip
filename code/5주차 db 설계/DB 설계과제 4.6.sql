bec5d3ed68696b4de98c55a9828c70cc661405db
doubleyuu6-blip <doubleyuu6@gmail.com>
2026-04-06 16:24:06 +0900

    Add files via upload

---
+++ code/5주차 db 설계/DB 설계과제 4.6.sql
@@ -0,0 +1,27 @@
+DROP TABLE professor;
+CREAT TABLE professer(
+	professor_id int [pk],
+	professor_name varchar(100),
+	department varchar(100),
+	salary numeric,
+	salary_level numeric,
+	hire_date date
+
+CREATE TABLE Course ( 
+	course_id int,
+	section_id int,
+	professor_id int,
+	course_name varchar(100),
+	PRIMARY KEY (course_id, section_id ), --복합키
+	FOREGIN KEY (professor_id, 
+
+CREATE TABLE Enrollment (
+	student_id int,
+	course_int,
+	grade varchar(2),
+	points numeric, -- 99.65
+	enrolled_at DATE, 
+	PRIMARY KEY(student_id, course_id),
+	FOREGIN(student_id) REFERENCES student(student_id),
+	--FOREGIN KEY (course_Id) REFERENCES Course(course_id) -- Course의 복합키 때문에 오류
+	
